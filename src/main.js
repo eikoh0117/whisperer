d1cff67770f4ac35ffb3081b68933d326a2c2528
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-07 09:26:07 +0900

    Generate vue-js proj and introduce FontAwesomeicon

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUser, faSignOutAlt, faEllipsisV);
+
+Vue.component("fa", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");

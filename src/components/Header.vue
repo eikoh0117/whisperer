f5179119fdaba20218dbd0a2b0f13aedc6baa514
Hidemitsu Aoki <olympos0117@gmail.com>
2020-05-08 14:33:25 +0900

    Change header depend on whether user signin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,17 @@
     <router-link to="/">
       <h1>whisp.</h1>
     </router-link>
-    <div class="btns">
-      <button @click="signIn">
-        <fa icon="user" />
+    <div v-if="currentUser" class="btns">
+      <button
+        :style="'background-image: url(' + currentUser.photoURL + ')'"
+      ></button>
+      <button>
+        <fa icon="sign-out-alt" @click="signOut" />
+      </button>
+    </div>
+    <div v-else class="btns">
+      <button>
+        <fa icon="user" @click="signIn" />
       </button>
     </div>
   </header>
@@ -15,6 +23,16 @@
 import firebase from "firebase";
 import { auth } from "../main";
 export default {
+  data() {
+    return {
+      currentUser: {},
+    };
+  },
+  created() {
+    auth.onAuthStateChanged((user) => {
+      this.currentUser = user;
+    });
+  },
   methods: {
     signIn() {
       const provider = new firebase.auth.GoogleAuthProvider();
